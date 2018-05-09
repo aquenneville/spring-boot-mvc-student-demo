213313f706afba8730fd83da53b95ab59a614f3c
aquenneville <alainquenneville@gmail.com>
2018-05-09 22:19:41 +0100

    Add first version

--- src/test/java/github/aq/mvcstudentdemo/DemoMvcStudentApplicationTests.java
+++ src/test/java/github/aq/mvcstudentdemo/DemoMvcStudentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demomvc;
+package github.aq.mvcstudentdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoMvcApplicationTests {
+public class DemoMvcStudentApplicationTests {
 
 	@Test
 	public void contextLoads() {
